9dfad7e9462a26ed9b74f8ef65c157972e28dc47
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-07 11:51:04 +0530

    DSA: Find Disappeared Numbers via cyclic sort (O(n) time, O(1) space)

--- dsa/01-arrays-hashing/05-array-cyclic-sort/Answer.java
+++ dsa/01-arrays-hashing/05-array-cyclic-sort/Answer.java
@@ -4,15 +4,36 @@
 import java.util.*;
 
 public class Answer {
-
+    private void swap(int[] arr, int i , int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        int len=nums.length;
+        int i=0;
+        int home;
+        while(i<len){
+            home=nums[i]-1;
+            if(nums[i]!=nums[home]){
+                swap(nums,i,home);
+            }
+            else{
+                i++;
+            }
+        }
+
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(i=0;i<len;i++){
+            if(nums[i]!=i+1){
+                arr.add(i+1);
+            }
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
-        // TODO: test with the examples in QUESTION.md
-        // e.g.  System.out.println(new Answer()....);
+        System.out.println(new Answer().findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
     }
 }
 
