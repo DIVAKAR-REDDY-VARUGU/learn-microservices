3fcc0c26a3edd231f184a0c0409a375ce2aeb9e4
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-07 16:15:55 +0530

    DSA: Set Matrix Zeroes O(1)-space in-place marking; fix 1 buggy test case (expected zeroed wrong column)

--- dsa/01-arrays-hashing/09-set-matrix-zeroes-in-place-marking/Answer.java
+++ dsa/01-arrays-hashing/09-set-matrix-zeroes-in-place-marking/Answer.java
@@ -6,13 +6,40 @@ import java.util.*;
 public class Answer {
 
     public void setZeroes(int[][] matrix) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        int r=matrix.length;
+        int c=matrix[0].length;
+        boolean col0=false;
+        // marking flags 
+        for(int i=0;i<r;i++){
+            if(matrix[i][0]==0&&!col0)col0=true;
+            for(int j=1;j<c;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;     // making row start = 0
+                    matrix[0][j]=0;     // making col start = 0
+                }
+            }
+        }
+
+
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=1;j--){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(col0)matrix[i][0]=0;
+        }
+
     }
 
     public static void main(String[] args) {
-        // TODO: test with the examples in QUESTION.md
-        // e.g.  System.out.println(new Answer()....);
+        int [][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        new Answer().setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
 
--- dsa/01-arrays-hashing/09-set-matrix-zeroes-in-place-marking/Test.java
+++ dsa/01-arrays-hashing/09-set-matrix-zeroes-in-place-marking/Test.java
@@ -98,7 +98,7 @@ public class Test {
         check("wide single row no zero", new int[][]{{1,2,3,4,5,6}}, new int[][]{{1,2,3,4,5,6}});
         check("tall single col with zero mid", new int[][]{{1},{2},{0},{4}}, new int[][]{{0},{0},{0},{0}});
         check("zero at very first cell", new int[][]{{0,2,3},{4,5,6},{7,8,9}}, new int[][]{{0,0,0},{0,5,6},{0,8,9}});
-        check("bottom row all become zero", new int[][]{{1,2},{3,4},{0,6}}, new int[][]{{1,0},{3,0},{0,0}});
+        check("bottom row all become zero", new int[][]{{1,2},{3,4},{0,6}}, new int[][]{{0,2},{0,4},{0,0}});
         check("2x2 with both flags", new int[][]{{1,2},{3,0}}, new int[][]{{1,0},{0,0}});
         check("large value preserved no zero", new int[][]{{100,200},{300,400}}, new int[][]{{100,200},{300,400}});
 
