8f101dd976583f06cd8363ee1002b43728943695
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-09 14:27:45 +0530

    DSA: Longest Palindromic Substring via expand-from-center (O(n^2) time, O(1) space)

--- dsa/02-two-pointers/04-expanding-from-center-palindromes/Answer.java
+++ dsa/02-two-pointers/04-expanding-from-center-palindromes/Answer.java
@@ -6,13 +6,33 @@ import java.util.*;
 public class Answer {
 
     public String longestPalindrome(String s) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+
+        int oddStrLen,evenStrLen,maxLen=1,strStart=0;
+        for(int i=0;i<s.length();i++){
+            oddStrLen=palindrome(s,i,i);
+            evenStrLen=palindrome(s,i,i+1);
+            int len=Math.max(oddStrLen, evenStrLen);
+            if(maxLen<len){
+                maxLen=len;
+                strStart=i-((len-1)/2);         // remember this , you will forget this 
+            }
+        }
+        return s.substring(strStart,strStart+maxLen);
+    }
+    public int palindrome(String s, int i, int j){
+        while (i>=0&&j<s.length()) {
+            if(s.charAt(i)==s.charAt(j)){
+                i--;
+                j++;
+            }else {
+                break;
+            }
+        }
+        return j-i-1;       // Remember this too , 
     }
 
     public static void main(String[] args) {
-        // TODO: test with the examples in QUESTION.md
-        // e.g.  System.out.println(new Answer()....);
+        System.out.println(new Answer().longestPalindrome("racecar"));
     }
 }
 
