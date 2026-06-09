73957c71ad3feda6c7222eee3fcded922f65792f
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-09 17:54:34 +0530

    DSA: Reverse String via two-pointer in-place swap (O(n) time, O(1) space)

--- dsa/02-two-pointers/07-string-reversal/Answer.java
+++ dsa/02-two-pointers/07-string-reversal/Answer.java
@@ -4,10 +4,17 @@
 import java.util.*;
 
 public class Answer {
-
+    public void swap(char[] arr,int i,int j){
+        char t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
     public void reverseString(char[] s) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        int i=0;
+        int j=s.length-1;
+        while (i<j) {
+            swap(s, i++, j--);
+        }
     }
 
     public static void main(String[] args) {
