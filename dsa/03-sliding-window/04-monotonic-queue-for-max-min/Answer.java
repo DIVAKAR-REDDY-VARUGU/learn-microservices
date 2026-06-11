f69ecf2bc9a03430376a003e9fa8dc1e8330ee0c
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-11 21:18:20 +0530

    DSA: Sliding Window Maximum via monotonic deque (O(n) time, O(k) space)

--- dsa/03-sliding-window/04-monotonic-queue-for-max-min/Answer.java
+++ dsa/03-sliding-window/04-monotonic-queue-for-max-min/Answer.java
@@ -6,13 +6,33 @@ import java.util.*;
 public class Answer {
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        ArrayDeque<Integer> deq=new ArrayDeque<>();
+        int[] res=new int[nums.length-k+1];
+        for(int i=0;i<nums.length;i++){
+
+            checkAndRemoveSmallElements(deq, nums, i);
+            deq.add(i);
+            removeOldInd(deq,i,k);
+            if(i+1>=k){
+                res[i-k+1]=nums[deq.peekFirst()];
+            }
+        }
+        return res;
+
+    }
+
+    public void checkAndRemoveSmallElements(ArrayDeque<Integer> deq,int[] nums,int i){
+        while (!deq.isEmpty()&&nums[deq.peekLast()]<nums[i]) {
+            deq.pollLast();
+        }
+    }
+
+    public void removeOldInd(ArrayDeque<Integer> deq,int i,int k){
+        if(deq.peekFirst()<=i-k)deq.pollFirst();
     }
 
     public static void main(String[] args) {
-        // TODO: test with the examples in QUESTION.md
-        // e.g.  System.out.println(new Answer()....);
+         System.out.println(Arrays.toString(new Answer().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7},3)));
     }
 }
 
