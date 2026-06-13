7c61c2955ad795c0a869f65e1a5c842e24bd6c26
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-13 22:37:04 +0530

    DSA: Find First and Last Position via boundary binary searches (O(log n) time, O(1) space)

--- dsa/04-binary-search/02-find-first-last-occurrence/Answer.java
+++ dsa/04-binary-search/02-find-first-last-occurrence/Answer.java
@@ -6,8 +6,32 @@ import java.util.*;
 public class Answer {
 
     public int[] searchRange(int[] nums, int target) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        int s=0,e=nums.length-1,mid;
+        while (s<=e) {
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return new int[]{findBetween(nums,s,mid,target,true),findBetween(nums,mid,e,target,false)};
+            }else if(nums[mid]<target){
+                s=mid+1;
+            }else e=mid-1;
+            
+        }
+        return new int[]{-1,-1};
+    }
+    public int findBetween(int[] nums,int s,int e,int target,boolean min){
+        int mid;
+        while (s<=e) {
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                if(min)e=mid-1;
+                else s=mid+1;
+
+            }else if(nums[mid]<target){
+                s=mid+1;
+            }else e=mid-1;
+            
+        }
+        return min?s:e;
     }
 
     public static void main(String[] args) {
