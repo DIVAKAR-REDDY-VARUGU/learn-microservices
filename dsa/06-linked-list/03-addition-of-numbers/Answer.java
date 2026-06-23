f4cb65f37726c66791a5271fdba6d6ae67ea22d5
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-23 19:30:20 +0530

    DSA: Add Two Numbers digit-by-digit with carry (O(max(m,n)) time, O(max(m,n)) space)

--- dsa/06-linked-list/03-addition-of-numbers/Answer.java
+++ dsa/06-linked-list/03-addition-of-numbers/Answer.java
@@ -6,8 +6,26 @@ import java.util.*;
 public class Answer {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        ListNode head=new ListNode(0);
+        ListNode tail=head;
+        int carry=0;
+        while (l1!=null||l2!=null) {
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            sum+=carry;
+            carry=sum/10;
+            tail.next=new ListNode(sum%10);
+            tail=tail.next;
+        }
+        if(carry!=0)tail.next=new ListNode(carry);
+        return head.next;
     }
 
     public static void main(String[] args) {
