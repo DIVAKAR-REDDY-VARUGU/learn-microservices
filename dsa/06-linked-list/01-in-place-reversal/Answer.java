ba304fc4a4a04209556f282ace528526b3e0a8db
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-23 18:58:44 +0530

    DSA: Reverse Linked List iteratively in-place (O(n) time, O(1) space)

--- dsa/06-linked-list/01-in-place-reversal/Answer.java
+++ dsa/06-linked-list/01-in-place-reversal/Answer.java
@@ -6,8 +6,15 @@ import java.util.*;
 public class Answer {
 
     public ListNode reverseList(ListNode head) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        ListNode pre=null;
+        ListNode cur=head;
+        while (cur!=null) {
+            ListNode temp=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=temp;
+        }
+        return pre;
     }
 
     public static void main(String[] args) {
