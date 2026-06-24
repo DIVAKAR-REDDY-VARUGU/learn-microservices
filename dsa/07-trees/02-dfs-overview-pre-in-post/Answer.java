3d6acdf9d2c6988a8208bcaddc855e6759eb234f
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-24 11:10:56 +0530

    DSA: Binary Tree Postorder Traversal (recursive, O(n) time, O(h) stack)

--- dsa/07-trees/02-dfs-overview-pre-in-post/Answer.java
+++ dsa/07-trees/02-dfs-overview-pre-in-post/Answer.java
@@ -6,8 +6,18 @@ import java.util.*;
 public class Answer {
 
     public List<Integer> postorderTraversal(TreeNode root) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        List<Integer> res=new ArrayList<>();
+
+        postorderTraversal(root,res);
+        return res;
+    }
+
+    public void postorderTraversal(TreeNode root, List<Integer> res){
+        if(root==null)return;
+        postorderTraversal(root.left,res);
+        postorderTraversal(root.right,res);
+        res.add(root.val);
+
     }
 
     public static void main(String[] args) {
