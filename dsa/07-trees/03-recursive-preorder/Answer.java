127972ae6b344218e5bf44b56e03982ef2d19234
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-24 11:35:00 +0530

    DSA: Binary Tree Preorder Traversal (recursive, O(n) time, O(h) stack)

--- dsa/07-trees/03-recursive-preorder/Answer.java
+++ dsa/07-trees/03-recursive-preorder/Answer.java
@@ -6,8 +6,15 @@ import java.util.*;
 public class Answer {
 
     public List<Integer> preorderTraversal(TreeNode root) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        List<Integer> res=new ArrayList<>();
+        preorderTraversal(root,res);
+        return res;
+    }
+    public void preorderTraversal(TreeNode root,List<Integer> res){
+        if(root==null)return;
+        res.add(root.val);
+        preorderTraversal(root.left,res);
+        preorderTraversal(root.right,res);
     }
 
     public static void main(String[] args) {
