f2533e1e6d3201b54bd0dd46969525a57f03d316
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-24 10:59:24 +0530

    DSA: Binary Tree Level Order Traversal via DFS + level index (O(n) time, O(h) stack)

--- dsa/07-trees/01-level-order-traversal-bfs/Answer.java
+++ dsa/07-trees/01-level-order-traversal-bfs/Answer.java
@@ -6,8 +6,20 @@ import java.util.*;
 public class Answer {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        List<List<Integer>> store=new ArrayList<>();
+        storeValuesByLevel(root,store,0);
+        return store;
+    }
+
+    public void storeValuesByLevel(TreeNode node,List<List<Integer>> store,int level){
+        if(node==null)return;
+        if(store.size()<=level){
+            store.add(level, new ArrayList<>());
+        }
+
+        store.get(level).add(Integer.valueOf(node.val));
+        storeValuesByLevel(node.left,store,level+1);
+        storeValuesByLevel(node.right,store,level+1);
     }
 
     public static void main(String[] args) {
