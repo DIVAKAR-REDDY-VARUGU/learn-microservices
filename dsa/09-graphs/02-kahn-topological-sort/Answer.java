46dad3ac5ed2c97cdc7d8c13a2da932e1d78fd35
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-06-28 23:14:16 +0530

    DSA: Course Schedule II via Kahn's topological sort (O(V+E) time)

--- dsa/09-graphs/02-kahn-topological-sort/Answer.java
+++ dsa/09-graphs/02-kahn-topological-sort/Answer.java
@@ -6,8 +6,34 @@ import java.util.*;
 public class Answer {
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)graph.add(i,new ArrayList<>());
+        int[] inDegree=new int[numCourses];
+        for(int[] edge:prerequisites){
+            int pre=edge[1];
+            int course=edge[0];
+            graph.get(pre).add(Integer.valueOf(course));
+            inDegree[course]++;
+        }
+
+        ArrayDeque<Integer> queue=new ArrayDeque<>();
+        for(int i=0;i<inDegree.length;i++)if(inDegree[i]==0)queue.offer(Integer.valueOf(i));
+
+        int[] result=new int[numCourses];
+        int ind=0;
+
+        while(!queue.isEmpty()){
+            Integer preCourse=queue.poll();
+            result[ind++]=(int)preCourse;
+            ArrayList<Integer> courses=graph.get(preCourse);
+            for(Integer course:courses){
+                inDegree[(int)course]--;
+                if(inDegree[(int)course]==0)queue.offer(course);
+            }
+        }
+
+        return (ind==numCourses)?result:new int[0];
+
     }
 
     public static void main(String[] args) {
