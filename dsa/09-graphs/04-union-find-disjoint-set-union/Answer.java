08351b1970925c44e31eb018ff7012714e799a8f
Divakar Reddy Varugu <divakarreddyvarugu@gmail.com>
2026-07-09 14:36:43 +0530

    DSA: Number of Provinces via Union-Find (Disjoint Set Union)

--- dsa/09-graphs/04-union-find-disjoint-set-union/Answer.java
+++ dsa/09-graphs/04-union-find-disjoint-set-union/Answer.java
@@ -6,8 +6,32 @@ import java.util.*;
 public class Answer {
 
     public int findCircleNum(int[][] isConnected) {
-        // TODO: implement your solution
-        throw new UnsupportedOperationException("TODO");
+        int r=isConnected.length;
+        int c=isConnected[0].length;
+        int[] leadersMap=new int[r];
+        for(int i=0;i<r;i++)leadersMap[i]=i;
+
+        int province=r;
+
+        for(int i=0;i<r;i++){
+            for(int j=i+1;j<c;j++){
+                if(isConnected[i][j]==1){
+                    int iLeader=findLeader(leadersMap, i);
+                    int jLeader=findLeader(leadersMap, j);
+                    if(iLeader!=jLeader){
+                        leadersMap[jLeader]=iLeader;
+                        province--;
+                    }
+                }
+            }
+        }
+        return province;
+    }
+    public int findLeader(int[] leadersMap,int target){
+        while(leadersMap[target]!=target){
+            target=leadersMap[target];
+        }
+        return target;
     }
 
     public static void main(String[] args) {
